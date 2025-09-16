fd929ec7d3cfe2edfcc12010e30654fc1c812784
Ephirious <Ephirious@gmail.com>
2025-09-17 00:31:41 +0300

    feat(links): add links for web sites

---
+++ links.py
@@ -0,0 +1,6 @@
+#Links
+VOYAGER = "https://www.jpl.nasa.gov/missions/voyager-1"
+RFC_1149 = "https://datatracker.ietf.org/doc/html/rfc1149"
+UNICODE = "https://www.unicode.org/Public/16.0.0/ucd/UnicodeData.txt"
+BITCOIN = "https://github.com/0xMagnuz/Bitcoin-v0.1/raw/refs/heads/master/bitcoin0.1/src/main.cpp"
+ICBN = "https://freecomputerbooks.com/The-C-Programming-Language.html"
